5242fc84b7c7d59f8a09f8c64984f9033db9d161
Bryce Kahle <bryce.kahle@datadoghq.com>
2021-02-09 14:50:09 -0500

    Fix tests to use testConfig so it picks up runtime compilation flag

--- pkg/network/tracer/tracer_test.go
+++ pkg/network/tracer/tracer_test.go
@@ -1805,7 +1805,7 @@ func TestUnconnectedUDPSendIPv6(t *testing.T) {
 		t.Skip("IPv6 not enabled on host")
 	}
 
-	cfg := config.NewDefaultConfig()
+	cfg := testConfig()
 	cfg.CollectIPv6Conns = true
 	tr, err := NewTracer(cfg)
 	require.NoError(t, err)
